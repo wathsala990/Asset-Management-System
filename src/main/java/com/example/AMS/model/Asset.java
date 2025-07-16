0fcb3342a3a473970b5e957796983615acbcabac
Wathsala Bandara <wathsalabandara2001@gmail.com>
2025-07-16 18:43:53 +0530

    re write the code

--- src/main/java/com/example/AMS/model/Asset.java
+++ src/main/java/com/example/AMS/model/Asset.java
@@ -35,26 +35,70 @@ public class Asset {
     public void setName(String name) {
         this.name = name;
     }
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
-    public String getBrand() { return brand; }
-    public void setBrand(String brand) { this.brand = brand; }
-    public String getModel() { return model; }
-    public void setModel(String model) { this.model = model; }
-    public String getSpecification() { return specification; }
-    public void setSpecification(String specification) { this.specification = specification; }
-    public Date getPurchaseDate() { return purchaseDate; }
-    public void setPurchaseDate(Date purchaseDate) { this.purchaseDate = purchaseDate; }
-    public boolean isActivityStatus() { return activityStatus; }
-    public void setActivityStatus(boolean activityStatus) { this.activityStatus = activityStatus; }
-    public String getWarrantyId() { return warrantyId; }
-    public void setWarrantyId(String warrantyId) { this.warrantyId = warrantyId; }
-    public Date getWarrantyDate() { return warrantyDate; }
-    public void setWarrantyDate(Date warrantyDate) { this.warrantyDate = warrantyDate; }
-    public String getWarrantyPeriod() { return warrantyPeriod; }
-    public void setWarrantyPeriod(String warrantyPeriod) { this.warrantyPeriod = warrantyPeriod; }
-    public String getPurchaseStore() { return purchaseStore; }
-    public void setPurchaseStore(String purchaseStore) { this.purchaseStore = purchaseStore; }
-    public Location getLocation() { return location; }
-    public void setLocation(Location location) { this.location = location; }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getSpecification() {
+        return specification;
+    }
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+    public boolean isActivityStatus() {
+        return activityStatus;
+    }
+    public void setActivityStatus(boolean activityStatus) {
+        this.activityStatus = activityStatus;
+    }
+    public String getWarrantyId() {
+        return warrantyId;
+    }
+    public void setWarrantyId(String warrantyId) {
+        this.warrantyId = warrantyId;
+    }
+    public Date getWarrantyDate() {
+        return warrantyDate;
+    }
+    public void setWarrantyDate(Date warrantyDate) {
+        this.warrantyDate = warrantyDate;
+    }
+    public String getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+    public void setWarrantyPeriod(String warrantyPeriod) {
+        this.warrantyPeriod = warrantyPeriod;
+    }
+    public String getPurchaseStore() {
+        return purchaseStore;
+    }
+    public void setPurchaseStore(String purchaseStore) {
+        this.purchaseStore = purchaseStore;
+    }
+    public Location getLocation() {
+        return location;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
