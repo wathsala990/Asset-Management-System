b69424b0e3032909843119d43fc9e1d42a73c59c
Wathsala Bandara <wathsalabandara2001@gmail.com>
2025-07-29 14:10:05 +0530

    remove some files

--- src/main/java/com/example/AMS/repository/M_RoomRepository.java
+++ src/main/java/com/example/AMS/repository/M_RoomRepository.java
@@ -2,11 +2,10 @@ package com.example.AMS.repository;
 
 import com.example.AMS.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface M_RoomRepository extends JpaRepository<Room, String> {
-    List<Room> findByLocation_LocationId(String locationId);
+public interface M_RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByRoomName(String roomName);
+    List<Room> findByLocationLocationId(String locationId); // Correct method name
 }
