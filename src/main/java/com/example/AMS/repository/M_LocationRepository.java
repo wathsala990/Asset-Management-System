28d1d6b86a4a54bdc2075787b7a593b1d5bc169b
Wathsala Bandara <139883269+wathsala990@users.noreply.github.com>
2025-08-09 17:07:55 +0530

    update M_LocationRepository file

--- src/main/java/com/example/AMS/repository/M_LocationRepository.java
+++ src/main/java/com/example/AMS/repository/M_LocationRepository.java
@@ -4,6 +4,9 @@ import com.example.AMS.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface M_LocationRepository extends JpaRepository<Location, String> {
+    List<Location> findByDeletedFalse();
 }
