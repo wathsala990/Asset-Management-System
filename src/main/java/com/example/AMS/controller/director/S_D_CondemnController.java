4560f19d3c3fb09641cefc40c074e1932cf9cc28
Wathsala Bandara <139883269+wathsala990@users.noreply.github.com>
2025-09-12 22:53:25 +0530

    Create S_D_CondemnController.java

---
+++ src/main/java/com/example/AMS/controller/director/S_D_CondemnController.java
@@ -0,0 +1,30 @@
+package com.example.AMS.controller.director;
+
+import com.example.AMS.model.Asset;
+import com.example.AMS.model.Condemn;
+import com.example.AMS.service.S_CondemnService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/director/condemn")
+public class S_D_CondemnController {
+    @Autowired
+    private S_CondemnService condemnService;
+
+    @GetMapping("/assets")
+    public List<Asset> getAllAssets() {
+        return condemnService.getAllAssets();
+    }
+
+    @PostMapping("/condemn")
+    public String condemnAsset(@RequestBody Condemn condemn) {
+        return condemnService.condemnAsset(condemn);
+    }
+
+    @GetMapping("/condemnDetails")
+    public Condemn getCondemnDetails(@RequestParam String assetId) {
+        return condemnService.getLatestCondemnByAssetId(assetId);
+    }
+}
